d52b1e44e298b6e91e1af2e10552f35b3861b797
LambertoNeri <neri.lamberto91@gmail.com>
2023-06-19 18:48:37 +0200

    add a class and construct

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
 
+class Movie {
+    public $title;
+    public $duration;
+    public $type;
+
+    public function __construct($title, $duration, $type){
+        $this->title = $title;
+        $this->duration = $duration;
+        $this->type = $type;
+    }
+    
+        
+    
+}
